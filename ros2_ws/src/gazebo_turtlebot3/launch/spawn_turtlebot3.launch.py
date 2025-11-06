b6c9ab422799efe1e956ff1d5c148686406ad269
TakiRyo <takiryo00614@gmail.com>
2025-11-06 09:26:25 +0900

    finish gazebo part

--- ros2_ws/src/gazebo_turtlebot3/launch/spawn_turtlebot3.launch.py
+++ ros2_ws/src/gazebo_turtlebot3/launch/spawn_turtlebot3.launch.py
@@ -1,17 +1,3 @@
-# Copyright 2019 Open Source Robotics Foundation, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import os
 
 from ament_index_python.packages import get_package_share_directory
@@ -22,9 +8,6 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    # Get the urdf file
-    # TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
-    # model_folder = 'turtlebot3_' + TURTLEBOT3_MODEL
     urdf_path = os.path.join(
         get_package_share_directory('gazebo_turtlebot3'),
         'models',
